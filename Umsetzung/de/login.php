d16c1a637e55fcaafee8c262b39e741d798963dd
Henning Kahl <Henning.Kahl@web.de>
2015-04-21 21:48:41 +0200

    html - Erstellen einer Login Seite
    Erstellung einer Loginseite als Prototyp, vorerst ohne Funktionalität.

---
+++ Umsetzung/de/login.php
@@ -0,0 +1,24 @@
+<div class="content">
+	<div class="headline">Anmelden <span style="margin-left: 289px;">Registrieren</span></div><br />
+	<div class="text" style="margin-right: 0px;">
+		<div id="form">
+			<form action="index.php?section=login#form" method="post" onsubmit="">
+				<input name="session" type="hidden" value="" />
+				<p>
+					<label class="text" for="username">Benutzername</label><br />
+					<input id="username" name="username" value="" size="40" />
+				</p>
+				<p>
+					<label class="text" for="password">Passwort</label><br />
+					<input id="password" name="password" type="password" value="" size="40" />
+				</p>
+					<input id="submit" style="cursor: pointer !important;width: 303px;" type="submit" value="Anmelden" />
+				</p>
+			</form>
+		</div>
+	</div>
+	<div class="text" style="margin-left: 418px; position: absolute; top: 58px;">
+		Ich bin neu hier und möchte mich registrieren<br />
+		<a href="index.php?section=registrieren"><input style="cursor: pointer !important; width: 333px;" type="button" value="Registrieren" />
+	</div>
+</div>
