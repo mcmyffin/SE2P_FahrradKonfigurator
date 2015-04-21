85affb5e136b2e54a397ec5a3943590a5066e026
Henning Kahl <Henning.Kahl@web.de>
2015-04-21 22:02:06 +0200

    Text - Entfernen von Test Daten
    Entfernen von Prototypdaten.

--- Umsetzung/de/main.php
+++ Umsetzung/de/main.php
@@ -1,7 +1,20 @@
 <div class="content">
 	<div class="headline">News</div><br />
 	<div class="text">
-		Test.
+		Wir heißen Sie herzlich Willkommen zum Fahrradkonfigurator und freuen uns, dass Sie sich für uns entschieden haben.<br /><br />
+		Unser junges Team setzt alles daran Ihnen und Ihren persönlichen Wünschen bei der Erstellung Ihres individualisierten Fahrrads gerecht zu werden.<br /><br />
+		Mit uns haben Sie sich für ein zuverlässiges und hoch professionelles Unternehmen entschieden, dass Ihnen dabei helfen wird schon bald Ihren Traum vom Fahrrad in die Realität umzusetzen.<br /><br />
+		Hierbei hilft Ihnen unser Fahrradkonfigurator, ein von Spezialisten entwickeltes System, das Ihnen in nur wenigen Schritten & Klick's die Zusammenstellung Ihres persönlichen Fahrrads erlaubt.<br />
+		Anschließend bauen wir Ihr Fahrrad in unserer modernen Werkstatt zusammen und schicken es an Ihre Wunschadresse.<br /><br />
+		Sie können sich nicht nur zwischen verschiedenen Fahrradtypen, Größen und Farben entscheiden, sondern haben Sie auch die Möglichkeit diverse Reifen, Sättel sowie Schaltungen zu Ihrer Konfiguration hinzufügen.<br /><br />
+		Ob als Geschenk oder für Ihren persönlichen Gebrauch, der Fahrradkonfigurator und wir lassen Sie nicht im Stich!<br /><br />
+		Ihr Team von FahrradKonfigurator.de<br /><br />
+		<div class="text" style="margin-right: 0px;">
+			<a href="index.php?section=konfigurator"><input style="cursor: pointer !important; width: 350px;" type="button" value="Jetzt loslegen!" />
+		</div>
+		<div class="text" style="margin-left: 450px; position: absolute; top: 499px;">
+			<a href="index.php?section=kontakt"><input style="cursor: pointer !important; width: 350px;" type="button" value="Fragen / Wünsche?" />
+		</div>
 	</div>
 </div>
 
