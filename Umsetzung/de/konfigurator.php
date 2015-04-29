131eb5f4603d9b36073b7d0547615c3c51aea2b9
Henning Kahl <Henning.Kahl@web.de>
2015-04-29 19:01:39 +0200

    html/Text - Konfigurator Schritt 1 - Fahrradtyp
    Konfigurator Schritt 1 - Fahrradtyp muss mit einem Layout versehen,
    sowie mit einem kleinen Beschreibungstext oder ggf. rechtlichen
    Grundlagen ausgeschmückt werden.

--- Umsetzung/de/konfigurator.php
+++ Umsetzung/de/konfigurator.php
@@ -3,18 +3,21 @@
 		<table height="50px" width="850px">
 			<colgroup>
 			<?php
-			$count=8; //auslesen der Datein auch Namen mit php
+			$count=9; //auslesen der Datein auch Namen mit php
 			for($i=0; $i < $count; $i++){
-				echo "<col>";
-				echo "<col width=\"6px\">";
+				echo "\t\t\t\t<col>\n";
+				echo "\t\t\t\t<col width=\"6px\">\n";
 			}
 			?>
 			</colgroup>
 			<tr>
 			<?php
+			$array = ["Typ","Farbe","Größe","Gabel","Reifen","Schaltung","Sattel","Licht","Zubehör"];
 			for($i=0; $i < $count; $i++){
-				echo "<td align=\"center\"><b class=\"text\">test</b></td>";
-				echo "<td><img alt=\"|\" height=\"41px\" src=\"de/get/timeline-break.png\" /></td>";
+				echo "\t\t\t\t<td align=\"center\"><b class=\"text\">";
+				echo $array[$i];
+				echo "</b></td>\n";
+				echo "\t\t\t\t<td><img alt=\"|\" height=\"41px\" src=\"de/get/timeline-break.png\" /></td>\n";
 			}
 			?>
 			</tr>
@@ -22,13 +25,19 @@
 	</div>
 </div>
 <div id="view-bg">
-	<div id="view"></div>
+	<div id="view"><img alt="$php" height="366px" src="de/get/stadtrad/stadtrad-blau.png" width="591px"/></div>
 </div>
 <div id="infobox-bg">
-	<div id="infobox"></div>
+	<div id="infobox">
+		<b>Fahrradtyp:</b><br /><br />
+		Sie haben die Auswahl zwischen:<br /><br />
+		- <a href="">Crossrad</a><br />
+		- <a href="">Mountainbike</a><br />
+		- <a href="">Stadtrad</a><br /><br />
+		Im <a href="index.php?section=step02">nächsten Schritt</a> wählen Sie die Rahmenfarbe aus.<br /><br />
+	</div>
 </div>
 <div id="menu">
-	<div id="last"><a href="index.php?section=konfigurator"><img alt="Last" src="de/get/last.png" /></a></div>
 	<div id="info">
 		<div id="info-bg-left"></div>
 		<div id="info-bg-center"><div class="info">Aktueller Preis: php€</div></div>
--- Umsetzung/php/config.php
+++ Umsetzung/php/config.php
@@ -2,6 +2,7 @@
 $dateien = array();
 $dateien['account'] = "de/account.php";
 $dateien['agb'] = "de/agb.php";
+$dateien['bestellen'] = "de/bestellen.php";
 $dateien['impressum'] = "de/impressum.php";
 $dateien['konfigurator'] = "de/konfigurator.php";
 $dateien['kontakt'] = "de/kontakt.php";
@@ -9,5 +10,12 @@ $dateien['login'] = "de/login.php";
 $dateien['main'] = "de/main.php";
 $dateien['registrieren'] = "de/registrieren.php";
 $dateien['step02'] = "de/step02.php";
+$dateien['step03'] = "de/step03.php";
+$dateien['step04'] = "de/step04.php";
+$dateien['step05'] = "de/step05.php";
+$dateien['step06'] = "de/step06.php";
+$dateien['step07'] = "de/step07.php";
+$dateien['step08'] = "de/step08.php";
+$dateien['step09'] = "de/step09.php";
 $dateien['warenkorb'] = "de/warenkorb.php";
 ?>
