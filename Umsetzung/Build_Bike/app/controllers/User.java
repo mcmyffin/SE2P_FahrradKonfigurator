5800f53b051e8efd53f9ee5c036d924762350233
MirIsLangweilig <torben-dennis.mader@haw-hamburg.de>
2015-05-02 21:10:48 +0200

    Mock Mailserver + Mail Controller hinzugefügt. Der Mailcontroller kann bisher nur kontakt mails verschicken. Keine Kontrolle der Eingaben!

--- Umsetzung/Build_Bike/app/controllers/User.java
+++ Umsetzung/Build_Bike/app/controllers/User.java
@@ -24,16 +24,6 @@ public class User extends Controller {
         return ok(login.render(kunde.login(username,password)+""));
     }
 
-    public static Result mail(){
-        DynamicForm mailData = Form.form().bindFromRequest();
-        String emailKunde = "testmail@knfitest1234.konfi"; //mailData.get("");
-        String emailBetreff = "testauf funzt"; //mailData.get("");
-        String emailNachricht = "haltsmaul"; //mailData.get("");
-        IMailer kontaktMailer = new Mailer();
-        kontaktMailer.sendKontaktMail(emailKunde,emailBetreff,emailNachricht);
-        return ok(kontakt.render("<div class=\"success\">Deine Anfrage wurde gesendet.</div>"));
-    }
-
     public static Result logout(){
         // todo
         return null;
