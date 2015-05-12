fb0727469d1ed7a807344c21e7c01c6c42941be4
MirIsLangweilig <torben-dennis.mader@haw-hamburg.de>
2015-05-12 19:47:28 +0200

    Datanbank angepasst, der Adapter folgt

--- Umsetzung/FahrradKonfi.sql
+++ Umsetzung/FahrradKonfi.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 06. Mai 2015 um 15:04
--- Server Version: 10.0.17-MariaDB-1~trusty-log
+-- Erstellungszeit: 12. Mai 2015 um 19:36
+-- Server Version: 10.0.19-MariaDB-1~trusty-log
 -- PHP-Version: 5.5.9-1ubuntu4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `AdresseT` (
   `A_Stadt` varchar(32) NOT NULL,
   `A_adresszusatz` varchar(63) NOT NULL,
   PRIMARY KEY (`A_ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 --
 -- Daten für Tabelle `AdresseT`
@@ -42,7 +42,9 @@ CREATE TABLE IF NOT EXISTS `AdresseT` (
 
 INSERT INTO `AdresseT` (`A_ID`, `A_strasse`, `A_hausnummer`, `A_plz`, `A_Stadt`, `A_adresszusatz`) VALUES
 (1, 'Dada Straße', 1, 10000, 'Dadamstadt', 'Damnit!'),
-(4, 'Ja...hm.. Berliner tor glaub ich', 234, 20223, 'HambuuuuuuuurgNicht!', 'UzUz2');
+(4, 'Ja...hm.. Berliner tor glaub ich', 234, 20223, 'HambuuuuuuuurgNicht!', 'UzUz2'),
+(19, '123', 123, 123, '123', ''),
+(20, '213', 123, 123, '', '');
 
 -- --------------------------------------------------------
 
@@ -65,9 +67,34 @@ CREATE TABLE IF NOT EXISTS `FelgenT` (
   `F_ID` int(11) NOT NULL AUTO_INCREMENT,
   `F_bild` varchar(127) NOT NULL,
   `F_BESCH` int(11) NOT NULL,
+  `F_preis` int(11) NOT NULL,
+  `F_reifengroesse` int(3) NOT NULL,
+  `F_name` int(11) NOT NULL,
+  `F_narbendynamo` tinyint(1) NOT NULL,
+  `F_formTyp` int(11) NOT NULL,
+  `F_formTypID` int(11) NOT NULL,
   PRIMARY KEY (`F_ID`),
-  KEY `F_BESCH` (`F_BESCH`)
+  KEY `F_BESCH` (`F_BESCH`),
+  KEY `F_formTypID` (`F_formTypID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `FormTypT`
+--
+
+CREATE TABLE IF NOT EXISTS `FormTypT` (
+  `Fo_ID` int(11) NOT NULL,
+  `Fo_gabel` int(11) NOT NULL,
+  `Fo_felgen` int(11) NOT NULL,
+  `Fo_vorbau` int(11) NOT NULL,
+  `Fo_mantel` int(11) NOT NULL,
+  `Fo_rahmen` int(11) NOT NULL,
+  `Fo_teile` int(11) NOT NULL,
+  `Fo_sattel` int(11) NOT NULL,
+  PRIMARY KEY (`Fo_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -79,8 +106,19 @@ CREATE TABLE IF NOT EXISTS `GabelT` (
   `G_ID` int(11) NOT NULL AUTO_INCREMENT,
   `G_bild` varchar(127) NOT NULL,
   `G_BESCH` int(11) NOT NULL,
+  `G_formTyp` int(11) NOT NULL,
+  `G_formTypID` int(11) NOT NULL,
+  `G_schaftlaenge` int(11) NOT NULL,
+  `G_steuersatz` int(11) NOT NULL,
+  `G_felgenbrems` tinyint(1) NOT NULL,
+  `G_scheibenbrems` tinyint(1) NOT NULL,
+  `G_licht` tinyint(1) NOT NULL,
+  `G_reifengroesse` int(3) NOT NULL,
+  `G_preis` int(11) NOT NULL,
+  `G_name` int(11) NOT NULL,
   PRIMARY KEY (`G_ID`),
-  KEY `G_BESCH` (`G_BESCH`)
+  KEY `G_BESCH` (`G_BESCH`),
+  KEY `G_formTypID` (`G_formTypID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -102,7 +140,7 @@ CREATE TABLE IF NOT EXISTS `KundeT` (
   PRIMARY KEY (`K_ID`),
   UNIQUE KEY `K_email` (`K_email`),
   KEY `K_adresse` (`K_adresse`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 --
 -- Daten für Tabelle `KundeT`
@@ -110,7 +148,9 @@ CREATE TABLE IF NOT EXISTS `KundeT` (
 
 INSERT INTO `KundeT` (`K_ID`, `K_vorname`, `K_nachname`, `K_passwort`, `K_email`, `K_adresse`, `K_telefon`, `K_gebdatum`, `K_anrede`) VALUES
 (1, 'Dada', 'Dumdum', 'kenntKeiner', 'dada@dumdum.de', 1, 123, '', ''),
-(2, 'Herbert', 'Lappen', 'wie Passwort?', 'MeineMail@Da.bitte', 4, 2147483647, '', '');
+(2, 'Herbert', 'Lappen', 'wie Passwort?', 'MeineMail@Da.bitte', 4, 2147483647, '', ''),
+(9, 'alkjnd', 'aslkdn', 'lkjand', 'asjdn', 19, 1231, 'asldkn', ''),
+(10, '', '', '', '', 20, 0, '', '');
 
 -- --------------------------------------------------------
 
@@ -122,8 +162,15 @@ CREATE TABLE IF NOT EXISTS `MantelT` (
   `M_ID` int(11) NOT NULL AUTO_INCREMENT,
   `M_bild` varchar(127) NOT NULL,
   `M_BESCH` int(11) NOT NULL,
+  `M_name` varchar(128) NOT NULL,
+  `M_formTyp` int(11) NOT NULL,
+  `M_formTypID` int(11) NOT NULL,
+  `M_durchm` int(11) NOT NULL,
+  `M_reifengroesse` int(3) NOT NULL,
+  `M_preis` int(11) NOT NULL,
   PRIMARY KEY (`M_ID`),
-  KEY `M_BESCH` (`M_BESCH`)
+  KEY `M_BESCH` (`M_BESCH`),
+  KEY `M_formTypID` (`M_formTypID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -136,11 +183,25 @@ CREATE TABLE IF NOT EXISTS `RahmenT` (
   `R_ID` int(11) NOT NULL AUTO_INCREMENT,
   `R_hoehe` int(3) NOT NULL,
   `R_form` varchar(32) NOT NULL,
-  `R_reifengröße` int(3) NOT NULL,
+  `R_reifengroesse` int(3) NOT NULL,
   `R_bild` varchar(127) NOT NULL,
   `R_BESCH` int(11) NOT NULL,
+  `R_steuersatz` int(11) NOT NULL,
+  `R_schafthoehe` int(11) NOT NULL,
+  `R_zusatzbefest` int(3) NOT NULL,
+  `R_scheibenbrems` tinyint(1) NOT NULL,
+  `R_felgenbrems` tinyint(1) NOT NULL,
+  `R_tretlager` int(11) NOT NULL,
+  `R_formTyp` int(11) NOT NULL,
+  `R_formTypID` int(11) NOT NULL,
+  `R_name` varchar(128) NOT NULL,
+  `R_preis` int(11) NOT NULL,
+  `R_gepaektraeger` tinyint(1) NOT NULL,
+  `R_licht` tinyint(1) NOT NULL,
   PRIMARY KEY (`R_ID`),
-  KEY `R_BESCH` (`R_BESCH`)
+  KEY `R_BESCH` (`R_BESCH`),
+  KEY `R_formTypID` (`R_formTypID`),
+  KEY `R_formTypID_2` (`R_formTypID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -153,8 +214,13 @@ CREATE TABLE IF NOT EXISTS `SattelT` (
   `S_ID` int(11) NOT NULL AUTO_INCREMENT,
   `S_bild` varchar(127) NOT NULL,
   `S_BESCH` int(11) NOT NULL,
+  `S_name` varchar(128) NOT NULL,
+  `S_preis` int(11) NOT NULL,
+  `S_formTyp` int(11) NOT NULL,
+  `S_formTypID` int(11) NOT NULL,
   PRIMARY KEY (`S_ID`),
-  KEY `S_BESCH` (`S_BESCH`)
+  KEY `S_BESCH` (`S_BESCH`),
+  KEY `S_formTypID` (`S_formTypID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -167,8 +233,14 @@ CREATE TABLE IF NOT EXISTS `TeileT` (
   `T_ID` int(11) NOT NULL AUTO_INCREMENT,
   `T_bild` varchar(127) NOT NULL,
   `T_BESCH` int(11) NOT NULL,
+  `T_name` varchar(128) NOT NULL,
+  `T_preis` int(11) NOT NULL,
+  `T_zusatzBelegt` tinyint(1) NOT NULL,
+  `T_formTyp` int(11) NOT NULL,
+  `T_formTypID` int(11) NOT NULL,
   PRIMARY KEY (`T_ID`),
-  KEY `T_BESCH` (`T_BESCH`)
+  KEY `T_BESCH` (`T_BESCH`),
+  KEY `T_formTypID` (`T_formTypID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -181,8 +253,13 @@ CREATE TABLE IF NOT EXISTS `VorbauT` (
   `V_ID` int(11) NOT NULL AUTO_INCREMENT,
   `V_bild` varchar(127) NOT NULL,
   `V_BESCH` int(11) NOT NULL,
+  `V_preis` int(11) NOT NULL,
+  `V_schaftgroesse` int(11) NOT NULL,
+  `V_formTyp` int(11) NOT NULL,
+  `V_formTypID` int(11) NOT NULL,
   PRIMARY KEY (`V_ID`),
-  KEY `V_BESCH` (`V_BESCH`)
+  KEY `V_BESCH` (`V_BESCH`),
+  KEY `V_formTypID` (`V_formTypID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
@@ -193,13 +270,15 @@ CREATE TABLE IF NOT EXISTS `VorbauT` (
 -- Constraints der Tabelle `FelgenT`
 --
 ALTER TABLE `FelgenT`
-  ADD CONSTRAINT `FelgenT_ibfk_1` FOREIGN KEY (`F_BESCH`) REFERENCES `BeschreibungT` (`B_ID`) ON DELETE CASCADE;
+  ADD CONSTRAINT `FelgenT_ibfk_1` FOREIGN KEY (`F_BESCH`) REFERENCES `BeschreibungT` (`B_ID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FelgenT_ibfk_2` FOREIGN KEY (`F_formTypID`) REFERENCES `FormTypT` (`Fo_ID`);
 
 --
 -- Constraints der Tabelle `GabelT`
 --
 ALTER TABLE `GabelT`
-  ADD CONSTRAINT `GabelT_ibfk_1` FOREIGN KEY (`G_BESCH`) REFERENCES `BeschreibungT` (`B_ID`) ON DELETE NO ACTION;
+  ADD CONSTRAINT `GabelT_ibfk_1` FOREIGN KEY (`G_BESCH`) REFERENCES `BeschreibungT` (`B_ID`) ON DELETE NO ACTION,
+  ADD CONSTRAINT `GabelT_ibfk_2` FOREIGN KEY (`G_formTypID`) REFERENCES `FormTypT` (`Fo_ID`);
 
 --
 -- Constraints der Tabelle `KundeT`
@@ -211,31 +290,36 @@ ALTER TABLE `KundeT`
 -- Constraints der Tabelle `MantelT`
 --
 ALTER TABLE `MantelT`
-  ADD CONSTRAINT `MantelT_ibfk_1` FOREIGN KEY (`M_BESCH`) REFERENCES `BeschreibungT` (`B_ID`);
+  ADD CONSTRAINT `MantelT_ibfk_1` FOREIGN KEY (`M_BESCH`) REFERENCES `BeschreibungT` (`B_ID`),
+  ADD CONSTRAINT `MantelT_ibfk_2` FOREIGN KEY (`M_formTypID`) REFERENCES `FormTypT` (`Fo_ID`);
 
 --
 -- Constraints der Tabelle `RahmenT`
 --
 ALTER TABLE `RahmenT`
-  ADD CONSTRAINT `RahmenT_ibfk_1` FOREIGN KEY (`R_BESCH`) REFERENCES `BeschreibungT` (`B_ID`);
+  ADD CONSTRAINT `RahmenT_ibfk_1` FOREIGN KEY (`R_BESCH`) REFERENCES `BeschreibungT` (`B_ID`),
+  ADD CONSTRAINT `RahmenT_ibfk_2` FOREIGN KEY (`R_formTypID`) REFERENCES `FormTypT` (`Fo_ID`);
 
 --
 -- Constraints der Tabelle `SattelT`
 --
 ALTER TABLE `SattelT`
-  ADD CONSTRAINT `SattelT_ibfk_1` FOREIGN KEY (`S_BESCH`) REFERENCES `BeschreibungT` (`B_ID`);
+  ADD CONSTRAINT `SattelT_ibfk_1` FOREIGN KEY (`S_BESCH`) REFERENCES `BeschreibungT` (`B_ID`),
+  ADD CONSTRAINT `SattelT_ibfk_2` FOREIGN KEY (`S_formTypID`) REFERENCES `FormTypT` (`Fo_ID`);
 
 --
 -- Constraints der Tabelle `TeileT`
 --
 ALTER TABLE `TeileT`
-  ADD CONSTRAINT `TeileT_ibfk_1` FOREIGN KEY (`T_BESCH`) REFERENCES `BeschreibungT` (`B_ID`);
+  ADD CONSTRAINT `TeileT_ibfk_1` FOREIGN KEY (`T_BESCH`) REFERENCES `BeschreibungT` (`B_ID`),
+  ADD CONSTRAINT `TeileT_ibfk_2` FOREIGN KEY (`T_formTypID`) REFERENCES `FormTypT` (`Fo_ID`);
 
 --
 -- Constraints der Tabelle `VorbauT`
 --
 ALTER TABLE `VorbauT`
-  ADD CONSTRAINT `VorbauT_ibfk_1` FOREIGN KEY (`V_BESCH`) REFERENCES `BeschreibungT` (`B_ID`);
+  ADD CONSTRAINT `VorbauT_ibfk_1` FOREIGN KEY (`V_BESCH`) REFERENCES `BeschreibungT` (`B_ID`),
+  ADD CONSTRAINT `VorbauT_ibfk_2` FOREIGN KEY (`V_formTypID`) REFERENCES `FormTypT` (`Fo_ID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
