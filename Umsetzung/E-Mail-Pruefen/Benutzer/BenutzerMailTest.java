b476ed7481bf33a4bfdbc09140755e0bfd6d6171
Abdel <a.aouam@hotmail.de>
2015-05-16 20:06:17 +0200

    Regulaere Ausdruck aktualisieren

--- Umsetzung/E-Mail-Pruefen/Benutzer/BenutzerMailTest.java
+++ Umsetzung/E-Mail-Pruefen/Benutzer/BenutzerMailTest.java
@@ -27,14 +27,22 @@ public class BenutzerMailTest {
 
 	@Test
 	public void testGueltigeEMail() {
-		assertTrue(BenutzerMail.GueltigeEMail("test@test.test"));
+		
+		assertTrue(BenutzerMail.GueltigeEMail("m_e-i_ne_e-m-ail@hotmail.de"));
+		assertTrue(BenutzerMail.GueltigeEMail("email1475@hotmail.de"));
+		assertTrue(BenutzerMail.GueltigeEMail("EMAIL@hotmail.de"));
+		assertTrue(BenutzerMail.GueltigeEMail("meine_email@hotmail.de"));
+
+
+
 		assertFalse(BenutzerMail.GueltigeEMail(null)); 
- 		assertFalse(BenutzerMail.GueltigeEMail("test")); 
- 		assertFalse(BenutzerMail.GueltigeEMail("test@@")); 
- 		assertFalse(BenutzerMail.GueltigeEMail("test.test")); 
+ 		assertFalse(BenutzerMail.GueltigeEMail("test@@hotmail.com")); 
+ 		assertFalse(BenutzerMail.GueltigeEMail("test@hotmail;com")); 
+ 		assertFalse(BenutzerMail.GueltigeEMail("test@test")); 
  		assertFalse(BenutzerMail.GueltigeEMail("www.test.com")); 
  		 
  		 
 	}
 
 }
+
