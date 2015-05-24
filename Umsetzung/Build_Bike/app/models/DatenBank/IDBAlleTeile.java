39cc7c5260176b0a661329689cc02248821e1cec
MirIsLangweilig <torben-dennis.mader@haw-hamburg.de>
2015-05-24 18:19:44 +0200

    Zusammenfassung der Teile interfaces

---
+++ Umsetzung/Build_Bike/app/models/DatenBank/IDBAlleTeile.java
@@ -0,0 +1,7 @@
+package models.DatenBank;
+
+/**
+ * Created by tin on 24.05.15.
+ */
+public interface IDBAlleTeile extends IDBFelgen, IDBGabel, IDBMantel, IDBRahmen, IDBSattel, IDBTeile, IDBVorbau  {
+}
