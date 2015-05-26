b0f8226adc39204e24b1da12b409eaf76cebe7db
mcmyffin <dimitri.meier@haw-hamburg.de>
2015-05-26 17:06:34 +0200

    DatenTyp Beschreibung hinzugefuegt

--- Umsetzung/Build_Bike/app/models/TeileKomponente/Einzelteile/Beschreibung.java
+++ Umsetzung/Build_Bike/app/models/TeileKomponente/Einzelteile/Beschreibung.java
@@ -9,6 +9,11 @@ public class Beschreibung {
 
     private Pair<String,String> beschreibungsPaar;
 
+    /**
+     *
+     * @param kurzbeschreibung
+     * @param werbetext
+     */
     public Beschreibung(String kurzbeschreibung, String werbetext){
         if(kurzbeschreibung == null) throw new NullPointerException("Kurzbeschreibung darf nicht null sein !");
         if(werbetext== null) throw new NullPointerException("Werbetext darf nicht null sein !");
@@ -16,10 +21,22 @@ public class Beschreibung {
         beschreibungsPaar = new Pair<String,String>(kurzbeschreibung,werbetext);
     }
 
+    /**
+     * Get Kurzbeschreibung
+     * Gibt die Kurzbeschreibung aus
+     *
+     * @return kurzbeschreibung
+     */
     public String getKurzbeschreibung(){
         return beschreibungsPaar.getKey();
     }
 
+    /**
+     * Get Werbetext
+     * Gibt den Werbetext aus
+     *
+     * @return werbetext
+     */
     public String getWerbetext(){
         return beschreibungsPaar.getValue();
     }
