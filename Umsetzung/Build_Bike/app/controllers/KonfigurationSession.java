73e282cc12a8c007695aae6dfa64f203dd6e04cc
mcmyffin <dimitri.meier@haw-hamburg.de>
2015-06-16 13:19:54 +0200

    Temporaere aenderung als MokupSession

--- Umsetzung/Build_Bike/app/controllers/KonfigurationSession.java
+++ Umsetzung/Build_Bike/app/controllers/KonfigurationSession.java
@@ -9,6 +9,10 @@ import models.KonfigurationKomponente.KonfigurationKomponente;
 import play.libs.Json;
 import play.mvc.Controller;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 /**
  * Created by dima on 13.06.15.
  */
@@ -16,23 +20,27 @@ public class KonfigurationSession extends Controller {
 
     // Session Konstante fuer temp Konfiguration
     private static final String KONFIGURATION = "TMP_KONFIGURATION";
-
+    private static Map<String,String> mokupSession = new HashMap();
 
     public static void setKonfigurationInSession(KonfigurationDTO dto){
 
         JsonNode dto_Json = Json.toJson(dto);
-
-        session(KONFIGURATION,dto_Json.toString());
+        System.err.println("speichere in Session: "+dto.getStep_10());
+        mokupSession.put(KONFIGURATION,dto_Json.toString());
+//        session(KONFIGURATION,dto_Json.toString());
     }
 
     public static KonfigurationDTO getKonfigurationFromSession(){
 
-        if(!session().containsKey(KONFIGURATION)) return null;
+//        if(!session().containsKey(KONFIGURATION)) return null;
+        if(!mokupSession.containsKey(KONFIGURATION)) return null;
 
-        String dto_Json_String = session(KONFIGURATION);
+//        String dto_Json_String = session(KONFIGURATION);
+        String dto_Json_String = mokupSession.get(KONFIGURATION);
+
         JsonNode dto_Json = Json.parse(dto_Json_String);
-
         KonfigurationDTO dto = Json.fromJson(dto_Json, KonfigurationDTO.class);
+        System.out.println("lade aus Session: "+dto.getStep_10());
 
         return dto;
     }
